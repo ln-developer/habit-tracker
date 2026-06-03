49a445f431b5d5c5a46424e31fae9feb5ca96740
Лиана Ниязметова <l.niyazmetova@big3.ru>
2026-06-03 23:16:41 +0300

    feat: connect habit form to store

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import { useState } from "react";
 
 import Modal from "./components/Modal";
 import HabitForm from "./components/HabitForm";
+import type { HabitFormState, HabitType } from "./types/habit.types";
+import { useHabitStore } from "./store/habitStore";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const { addHabit } = useHabitStore();
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleSubmit = (formState: HabitFormState) => {
+    const habit: HabitType = {
+      ...formState,
+      id: crypto.randomUUID(),
+      createdAt: new Date().toISOString(),
+      completions: [],
+    };
+
+    addHabit(habit);
+    setIsFormOpen(false);
+  };
 
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Open modal</button>
+      <button onClick={() => setIsFormOpen(true)}>Open modal</button>
 
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title="New habit">
-        <HabitForm habit={null} onSubmit={() => {}} />
+      <Modal
+        isOpen={isFormOpen}
+        onClose={() => setIsFormOpen(false)}
+        title="New habit"
+      >
+        <HabitForm habit={null} onSubmit={handleSubmit} />
       </Modal>
     </>
   );
